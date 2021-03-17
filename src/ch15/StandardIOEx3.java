259bd09ddf96b2dcdf77cb5b3411e872929c4a60
skysamer <skyslayer123@naver.com>
2021-03-17 21:15:07 +0900

    standardIO1

---
+++ src/ch15/StandardIOEx3.java
@@ -0,0 +1,24 @@
+package ch15;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class StandardIOEx3 {
+	public static void main(String[] args) {
+		PrintStream ps=null;
+		FileOutputStream fos=null;
+		
+		try {
+			fos=new FileOutputStream("test.txt");
+			ps=new PrintStream(fos);
+			System.setOut(ps);
+		}catch (FileNotFoundException e) {
+			System.err.println("File not found.");
+		}
+		System.out.println("Hello by System.out");
+		System.err.println("Hello by system.err");
+
+	}
+
+}
